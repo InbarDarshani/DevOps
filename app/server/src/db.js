9587b46e2c27e489e82a571c99f2c28db77fbab1
InbarDarshani <InbarProjects@gmail.com>
2022-08-02 19:14:43 +0300

    App fixes

--- app/server/src/db.js
+++ app/server/src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017';
+const url = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
 
 mongoose.connect(url + '/tasks', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Database connected successfully!'))

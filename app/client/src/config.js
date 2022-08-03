e5ce3e9c0d5972f64ce1ebdac97c15bce929df61
InbarDarshani <InbarProjects@gmail.com>
2022-08-03 04:48:44 +0300

    App fix

--- app/client/src/config.js
+++ app/client/src/config.js
@@ -1,1 +1,8 @@
-export const API_BASE_URL = process.env.API || 'http://localhost:3000'; 
+let base_url = window.location;
+
+if (process.env.NODE_ENV === "development")
+    base_url = 'http://localhost:3000';
+else
+    base_url = process.env.API;
+
+export const API_BASE_URL = base_url;
